0b824c16eebfc7adc17b1d129db4e61d17d06622
NazzalaIhsan217 <75056294+NazzalaIhsan217@users.noreply.github.com>
2021-10-04 20:53:52 +0700

    Add files via upload

---
+++ project1.ino
@@ -0,0 +1,13 @@
+void setup() {
+  /**/
+  Serial.begin(115200);
+  Serial.println("Welcome to idn");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Welcome to IDN");
+  delay(1000);
+
+}
